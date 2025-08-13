3a96bccd5322b79376bd25a0ae79bc9d155b34ab
Guy <guy.singer1@gmail.com>
2025-08-13 18:08:45 +0300

    introducing genericpage

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,21 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "../components/Header";
 import Home from "../features/home/Home";
-import Page from "./Page.tsx";
+import GenericPage from "./GenericPage.tsx";
 import "../App.css";
 
 const App: React.FC = () => {
   return (
     <>
-      <Header /> {/* Show header on all pages */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/page1" element={<Page title="Page 1" />} />
-        <Route path="/page2" element={<Page title="Page 2" />} />
-        <Route path="/page3" element={<Page title="Page 3" />} />
-        <Route path="/page4" element={<Page title="Page 4" />} />
-        <Route path="/page5" element={<Page title="Page 5" />} />
-        <Route path="/page6" element={<Page title="Page 6" />} />
+        <Route path="/page1" element={<GenericPage/>} />
+        <Route path="/page2" element={<GenericPage/>} />
+        <Route path="/page3" element={<GenericPage/>} />
+        <Route path="/page4" element={<GenericPage/>} />
+        <Route path="/page5" element={<GenericPage/>} />
+        <Route path="/page6" element={<GenericPage/>} />
       </Routes>
     </>
   );
---
+++ frontend/src/features/PortConfig/PortConfig.tsx
@@ -0,0 +1,11 @@
+import GenericPage from "../../app/GenericPage";
+
+const Page1: React.FC = () => {
+  return (
+    <GenericPage title="Page 1">
+      <p>This is the content of Page 1.</p>
+    </GenericPage>
+  );
+};
+
+export default Page1;
