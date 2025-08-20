834b113d38d7cd998129a6643d5372de95072577
Guy <guy.singer1@gmail.com>
2025-08-20 17:33:09 +0300

    changed to masonry grid

--- frontend/src/components/GridBlock.tsx
+++ frontend/src/components/GridBlock.tsx
@@ -2,12 +2,18 @@ interface GridBlockProps {
   title: string;
   logo: string;
   onClick: () => void;
+  isComingSoon?: boolean;
 }
 
-const GridBlock: React.FC<GridBlockProps> = ({ title, logo, onClick }) => {
+const GridBlock: React.FC<GridBlockProps> = ({ title, logo, onClick, isComingSoon = false }) => {
   return (
-    <div className="grid-block" onClick={onClick}>
-      <img src={logo} alt={title} className="grid-logo" />
+    <div 
+      className={`grid-block ${isComingSoon ? 'coming-soon' : ''}`} 
+      onClick={onClick}
+    >
+      {!isComingSoon && logo && (
+        <img src={logo} alt={title} className="grid-logo" />
+      )}
       <h2>{title}</h2>
     </div>
   );
