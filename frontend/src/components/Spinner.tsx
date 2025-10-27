2795ac0603c23f4de25ab103345472838f82034b
Guy <guy.singer1@gmail.com>
2025-10-27 15:15:56 +0200

    fixed spinner position

--- frontend/src/components/Spinner.tsx
+++ frontend/src/components/Spinner.tsx
@@ -6,7 +6,7 @@ type Props = {
     color?: string;
 }
 
-const spinner = ({isLoading = true, size = 30, color = '#555eb3ff'}: Props) => {
+const spinner = ({isLoading = true, size = 30, color = '#57e5f5'}: Props) => {
     if (!isLoading) return null;
   return (
     <ClipLoader
